c6ece7a4d5233e8a05b03152c0389eaaa40fddf2
VelociraptorSR <your-github-saumyarajpoot07@gmail.com>
2026-09-04 18:11:57 +0530

    Day 05 assignment

--- day01_assignment/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    inventory = {"Python Basics": 10, "Learning AI": 5}
-    book_title = input("enter the title of book: ")
-    quantity = input("enter the quantity of books: ")
-    action = input("enter the action that you want to do (sell, add, lookup): ")
-    print(type(quantity))
-    print(type(inventory[book_title]))
-    print(inventory[book_title])
